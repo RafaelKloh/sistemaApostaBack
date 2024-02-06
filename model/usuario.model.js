500399750f560faf4c549a04792e7961cb7f6729
RafaelKloh <rafaelkloh00@gmail.com>
2024-02-06 19:01:49 -0300

    verificacao se email ja esta cadastrado

--- model/usuario.model.js
+++ model/usuario.model.js
@@ -1,25 +1,44 @@
 import { rejects } from "assert"
-import {conexao} from "../conexao.js"
+import { conexao } from "../conexao.js"
 import { resolve } from "path"
 
 
 
-export async function cadastrar(nome,email,senha){  
-    return new Promise ((resolve,rejects)=>{
-        conexao.query(`INSERT INTO usuario VALUES(null, '${nome}','${email}','${senha}',1)`,(error,result)=>{
-            conexao.query(`SELECT * FROM usuario WHERE idUsuario = ${result.insertId}`,(erroSelect,resultSelect)=>{
-                resolve(resultSelect)
-            })
-        }) 
-    })
+export async function cadastrar(nome, email, senha) {
+    return new Promise((resolve, reject) => {
+        conexao.query(`SELECT * FROM usuario WHERE email = '${email}'`, (erroSelect, resultSelect) => {
+            if (erroSelect) {
+                reject(erroSelect);
+            } else {
+                if (resultSelect.length > 0) {
+                    const usuariojaCadastrado = { mensagem: "Email ja cadastrado" };
+                    resolve(usuariojaCadastrado);
+                } else {
+                    conexao.query(`INSERT INTO usuario VALUES(null, '${nome}','${email}','${senha}',1)`, (error, result) => {
+                        if (error) {
+                            reject(error);
+                        } else {
+                            conexao.query(`SELECT * FROM usuario WHERE idUsuario = ${result.insertId}`, (erroSelectUsuario, resultSelectUsuario) => {
+                                if (erroSelectUsuario) {
+                                    reject(erroSelectUsuario);
+                                } else {
+                                    resolve(resultSelectUsuario);
+                                }
+                            });
+                        }
+                    });
+                }
+            }
+        });
+    });
 }
 
-export async function apostaModell(idUsuario,numeroUsuario,dataCompleta,idFormaPagamento,valorAposta){  
-    return new Promise ((resolve,rejects)=>{
-        console.log(dataCompleta)
-        conexao.query(`INSERT INTO aposta VALUES(null, ${idUsuario},${numeroUsuario},'${dataCompleta}',${idFormaPagamento},${valorAposta})`,(error,result)=>{
+
+export async function apostaModell(idUsuario, numeroUsuario, dataCompleta, idFormaPagamento, valorAposta) {
+    return new Promise((resolve, rejects) => {
+        conexao.query(`INSERT INTO aposta VALUES(null, ${idUsuario},${numeroUsuario},'${dataCompleta}',${idFormaPagamento},${valorAposta})`, (error, result) => {
             console.log(error)
-           resolve(result)
-        }) 
+            resolve(result)
+        })
     })
 }
