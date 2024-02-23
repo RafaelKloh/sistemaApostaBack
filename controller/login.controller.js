31aa543fd3e42ebdea22a213e41fb6f1dd41fafa
djkadka <victorgneis@gmail.com>
2024-02-22 21:51:48 -0300

    ajustes finais eu espero

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -3,6 +3,7 @@ import {loginModel} from "../model/login.model.js"
 export async function login(req,res){
     const email = req.body.email
     const senha = req.body.senha
+    console.log(email,senha)
     const user = await loginModel(email,senha)
     return res.send(user)
 }
